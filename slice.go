cc7f732b377fd48ec8ccf5101a226a74a00ae6dd
jinzza456 <jinzza456@gmail.com>
2021-10-14 13:55:14 +0900

    slice bug

--- slice.go
+++ slice.go
@@ -38,27 +38,55 @@ func main() {
 	// fmt.Println(cap(a))
 	// fmt.Println(cap(b))
 
-	a := make([]int, 2, 4)
-	a[0] = 1
-	a[1] = 2
+	// a := make([]int, 2, 4)
+	// a[0] = 1
+	// a[1] = 2
 
-	b := make([]int, len(a))
+	// b := make([]int, len(a))
 
-	for i := 0; i < len(a); i++ {
-		b[i] = a[i]
+	// for i := 0; i < len(a); i++ {
+	// 	b[i] = a[i]
+	// }
+
+	// b = append(b, 3)
+
+	// fmt.Printf("%p %p\n", a, b)
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// b[0] = 4
+	// b[1] = 5
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	// b := a[4:8]
+	// fmt.Println(b)
+
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	// b := a[4:]
+	// fmt.Println(b)
+
+	// a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	// b := a[:4]
+	// fmt.Println(b)
+
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i := 0; i < 5; i++ {
+		var back int
+		a, back = RemoveBack(a)
+		fmt.Printf("%d, ", back)
 	}
-
-	b = append(b, 3)
-
-	fmt.Printf("%p %p\n", a, b)
-
+	fmt.Println()
 	fmt.Println(a)
-	fmt.Println(b)
+}
 
-	b[0] = 4
-	b[1] = 5
-
-	fmt.Println(a)
-	fmt.Println(b)
-
+func RemoveBack(a []int) ([]int, int) { // 여러개의 반환값이 나올땐 ()로 묶어준다.
+	return a[:len(a)-1], a[len(a)-1]
 }
