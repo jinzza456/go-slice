9b4e20b28183bd2f933c60677123d7e086723fd7
jinzza456 <jinzza456@gmail.com>
2021-10-14 13:19:26 +0900

    append

--- slice.go
+++ slice.go
@@ -3,15 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2, 3, 4, 5}
+	// a := make([]int, 2, 3)
+	// a[0] = 1
+	// a[1] = 2
 
-	fmt.Printf("len(a) = %d\n", len(a))
-	fmt.Printf("cap(a) = %d\n", cap(a))
+	// b := append(a, 3)
 
-	fmt.Println()
+	// fmt.Printf("%p %p\n", a, b)
 
-	b := make([]int, 0, 8)
+	// fmt.Println(a)
+	// fmt.Println(b)
 
-	fmt.Printf("len(a) = %d\n", len(b))
-	fmt.Printf("cap(a) = %d\n", cap(b))
+	// b[0] = 4
+	// b[1] = 5
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// a := []int{1, 2} // a는 2개의 공간만 있음
+
+	// b := append(a, 3) // a에 3을 추가
+
+	// fmt.Printf("%p %p\n", a, b)
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// b[0] = 4
+	// b[1] = 5
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// fmt.Println(cap(a))
+	// fmt.Println(cap(b))
+
+	a := make([]int, 2, 4)
+	a[0] = 1
+	a[1] = 2
+
+	b := make([]int, len(a))
+
+	for i := 0; i < len(a); i++ {
+		b[i] = a[i]
+	}
+
+	b = append(b, 3)
+
+	fmt.Printf("%p %p\n", a, b)
+
+	fmt.Println(a)
+	fmt.Println(b)
+
+	b[0] = 4
+	b[1] = 5
+
+	fmt.Println(a)
+	fmt.Println(b)
+
 }
